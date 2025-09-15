aab61c3a81cff6e2d8bbfe76b7511a68bbfcfbdd
ALIN <alin.kp025@gmail.com>
2025-09-16 00:58:10 +0530

    Create calculator2.py

---
+++ calculator2.py
@@ -0,0 +1,17 @@
+a=input("input first number= ")
+b=input("input second number= ")
+
+add=(int(a)+int(b))
+print("addition=",add)
+
+sub=(int(a)-int(b))
+print("substraction=",sub)
+
+mul=(int(a)*int(b))
+print("multiplication=",mul)
+
+div=(int(a)/int(b))
+print("division=",div)
+
+moddiv=(int(a)%int(b))
+print("modulo division=",moddiv)
