557b447237ec65e7f1f3db30885365ab7be6d777
ALIN <alin.kp025@gmail.com>
2025-09-16 00:58:06 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a=6
+b=3
+
+add=a+b
+print("addition=",add)
+
+sub=a-b
+print("substraction=",sub)
+
+mul=a*b
+print("multiplication=",mul)
+
+div=a/b
+print("division=",div)
+
+moddiv=a%b
+print("modulo division=",moddiv)
