c60aead2bc9840c74c75026086f21e6290a5cd1f
ALIN <alin.kp025@gmail.com>
2025-09-16 00:58:40 +0530

    Create example3.py

---
+++ example3.py
@@ -0,0 +1,14 @@
+a=int(input("enter the number a : "))
+b=int(input("enter another number b : "))
+
+c=a-b
+print("answer is : ",c)
+
+if(c<0):
+    print("b is grater than a")
+
+elif (c==0):
+    print("both numbers are same")
+
+else:
+    print("a is grater than b")
