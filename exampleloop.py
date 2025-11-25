26f8d4f7c84d3ac041f8a1711309f2ce089a798c
ALIN <alin.kp025@gmail.com>
2025-11-25 13:09:03 +0530

    Update exampleloop.py

--- exampleloop.py
+++ exampleloop.py
@@ -1,4 +1,9 @@
 i=int(input("enter the value: "))
-while (i<=10):
-    print(i)
-    i=i+1
+
+if i>10:
+    print("enter the valuse less than 10")
+
+else:
+    while i<=10:
+        print(i)
+        i=i+1
